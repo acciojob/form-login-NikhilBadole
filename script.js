2d5c1743d73c1fd50e28fb38f70cde7f22ae80d1
NikhilBadole <120246851+NikhilBadole@users.noreply.github.com>
2023-07-29 15:24:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let name = document.getElementsByTagName("input")[0];
+	let surname = document.getElementsByTagName("input")[1];
+	alert(`${name.value} ${surname.value}`);
 }
